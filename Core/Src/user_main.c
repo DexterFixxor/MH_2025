8e1a55d4f1ebe5914bfd7e1f805a52d7a214e214
DexterFixxor <48955668+DexterFixxor@users.noreply.github.com>
2025-12-26 12:30:30 +0100

    minors in user_main.c

--- Core/Src/user_main.c
+++ Core/Src/user_main.c
@@ -11,31 +11,23 @@
 #include "peripheries/gpio/gpio_it.h"
 #include "peripheries/encoder/encoder.h"
 #include "module/bdc_motor/bdc_motor.h"
+#include "module/odom/odom.h"
 
 #include "tim.h"
 #include "usart.h"
 #include <string.h>
 
-typedef enum
-{
-	NO_BLINK,
-	BLINK100MS,
-	BLINK500MS,
-	BLINK1000MS,
-}FSM_States_TypeDef;
+typedef enum {
+	NO_BLINK, BLINK100MS, BLINK500MS, BLINK1000MS,
+} FSM_States_TypeDef;
 
-
-
-void
-user_main()
-{
+void user_main() {
 	/* Init */
 
 	// Enable encoder interface on tim2 & tim3
 	HAL_TIM_Encoder_Start(&htim2, TIM_CHANNEL_ALL);
 	HAL_TIM_Encoder_Start(&htim3, TIM_CHANNEL_ALL);
 	encoders_init();
-
 
 	// Enable PWM on Motor1 & Motor2
 	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
@@ -46,8 +38,7 @@ user_main()
 
 	/* While petlja */
 
-	while (1)
-	{
+	while (1) {
 
 
 	}
